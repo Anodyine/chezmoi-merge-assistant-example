e0255107b2f9c000f1615b3bea389b0b6203dff8
Kyle Merritt <ky562730@ucf.edu>
2026-01-12 10:07:52 -0500

    updated monitors and synology startup

--- dot_config/hypr/conf/executable_synology-drive-start.sh
+++ dot_config/hypr/conf/executable_synology-drive-start.sh
@@ -7,19 +7,19 @@ sleep 10
 
 QT_QPA_PLATFORM=xcb /usr/bin/synology-drive "$@" >>"$LOGFILE" 2>&1 &
 
-sleep 5
+#sleep 5
 
-for i in {1..200}; do
-    addr=$(
-        /usr/bin/hyprctl -j clients 2>>"$LOGFILE" \
-        | /usr/bin/jq -r '.[] | select(.class=="cloud-drive-ui") | .address' 2>>"$LOGFILE" \
-        | head -n1
-    )
+# for i in {1..200}; do
+#     addr=$(
+#         /usr/bin/hyprctl -j clients 2>>"$LOGFILE" \
+#         | /usr/bin/jq -r '.[] | select(.class=="cloud-drive-ui") | .address' 2>>"$LOGFILE" \
+#         | head -n1
+#     )
 
-    if [ -n "$addr" ] && [ "$addr" != "null" ]; then
-        /usr/bin/hyprctl dispatch "movetoworkspacesilent 9,address:${addr}" >>"$LOGFILE" 2>&1
-        exit 0
-    fi
+#     if [ -n "$addr" ] && [ "$addr" != "null" ]; then
+#         /usr/bin/hyprctl dispatch "movetoworkspacesilent 9,address:${addr}" >>"$LOGFILE" 2>&1
+#         exit 0
+#     fi
 
-    sleep 0.1
-done
+#     sleep 0.1
+# done
